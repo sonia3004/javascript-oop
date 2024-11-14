dadd1b0d909c3bee69cee2733b2fb59e5cd92d2e
Arnaud MICHOUD <arnaud.michoud-ext@interflora.fr>
2024-11-14 22:38:48 +0100

    Persistence

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,8 +1,10 @@
 class Person {
   #name = "";
+  #type = "Person";
 
-  constructor(name) {
+  constructor(name, type) {
     this.#name = name;
+    this.#type = type;
   }
 
   get name() {
@@ -12,18 +14,41 @@ class Person {
   direBonjour() {
     return `Bonjour, je m'appelle ${this.#name}`;
   }
+
+  toJSON() {
+    return {
+      name: this.#name,
+      type: this.#type,
+    };
+  }
+
+  fromJSON(data) {
+    this.#name = data.name;
+  }
 }
 
 class Professor extends Person {
   #teaches = "";
 
   constructor(name, subject) {
-    super(name);
+    super(name, "Professor");
     this.#teaches = subject;
   }
 
   direBonjour() {
     return super.direBonjour() + " et j'enseigne " + this.#teaches;
+  }
+
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      teaches: this.#teaches,
+    };
+  }
+
+  fromJSON(data) {
+    super.fromJSON(data);
+    this.#teaches = data.teaches;
   }
 }
 
@@ -31,11 +56,23 @@ class Student extends Person {
   #year = "";
 
   constructor(name, year) {
-    super(name);
+    super(name, "Student");
     this.#year = year;
   }
 
   direBonjour() {
     return super.direBonjour() + " et je suis en année " + this.#year;
+  }
+
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      year: this.#year,
+    };
+  }
+
+  fromJSON(data) {
+    super.fromJSON(data);
+    this.#year = data.year;
   }
 }
