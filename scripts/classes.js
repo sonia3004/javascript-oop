7c582a4731c854a7954f2f37ce6b5f0072af2e2e
Arnaud MICHOUD <arnaud.michoud-ext@interflora.fr>
2024-11-17 23:42:33 +0100

    Courses manager

--- scripts/classes.js
+++ scripts/classes.js
@@ -76,3 +76,107 @@ class Student extends Person {
     this.#year = data.year;
   }
 }
+
+class Course {
+  #title = "";
+  #persons = [];
+
+  constructor(title) {
+    this.#title = title;
+  }
+
+  get title() {
+    return this.#title;
+  }
+
+  get persons() {
+    return this.#persons;
+  }
+
+  get students() {
+    return this.#persons.filter((person) => person instanceof Student);
+  }
+
+  get professor() {
+    return this.#persons.find((person) => person instanceof Professor);
+  }
+
+  set professor(prof) {
+    if (!prof instanceof Professor) {
+      throw "Seuls les professeurs sont autorisés à enseigner dans cette classe";
+    }
+
+    const profIndex = this.#persons.findIndex(
+      (person) => person instanceof Professor
+    );
+
+    if (profIndex !== -1) {
+      this.#persons[prodIndex] = prof;
+    } else {
+      this.#persons.push(prof);
+    }
+  }
+
+  addStudent(student) {
+    if (!student instanceof Student) {
+      throw "Seuls les étudiants sont autorisés à étudier dans cette classe";
+    }
+
+    if (this.#persons.includes(student)) {
+      throw "Cet étudiant est déjà inscrit à ce cours";
+    }
+
+    this.#persons.push(student);
+  }
+
+  removeStudent(student) {
+    const studentIndex = this.#persons.indexOf(student);
+
+    if (studentIndex === -1) {
+      throw "Cet étudiant n'est pas inscrit à ce cours";
+    }
+
+    this.#persons.splice(studentIndex, 1);
+  }
+
+  getStudentByName(name) {
+    return this.#persons.find((s) => s.name === name);
+  }
+
+  removeStudentByName(name) {
+    const student = this.getStudentByName(name);
+
+    this.removeStudent(student);
+  }
+
+  clearStudents() {
+    while (this.#persons.length > 0) {
+      this.#persons.pop();
+    }
+  }
+
+  fromJSON(data) {
+    this.#title = data.title;
+
+    this.#persons = data.persons.map((jsonPerson) => {
+      if (jsonPerson.type === "Professor") {
+        const professor = new Professor("");
+
+        professor.fromJSON(jsonPerson);
+        return professor;
+      }
+
+      const student = new Student("");
+
+      student.fromJSON(jsonPerson);
+      return student;
+    });
+  }
+
+  toJSON() {
+    return {
+      title: this.#title,
+      persons: this.#persons.map((person) => person.toJSON()),
+    };
+  }
+}
